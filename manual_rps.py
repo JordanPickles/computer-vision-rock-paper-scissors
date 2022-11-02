7f9a3184e4c626795cc3211095de92c8d56a6f96
JordanPickles <jordan.l.pickles@gmail.com>
2022-11-02 19:19:14 +0000

    Comments added to the manual_rps document

--- manual_rps.py
+++ manual_rps.py
@@ -1,25 +1,25 @@
 import random
 
 def get_computer_choice():
-    valid_answers = ["rock","paper","scissors"]
-    computer_choice = random.choice(valid_answers)
-    return computer_choice
+    valid_answers = ["rock","paper","scissors"] # Creates the string of possible choices
+    computer_choice = random.choice(valid_answers) # Random.choice function provides a random choice of the valid_answers variable
+    return computer_choice # Returns the output
     
 
 def get_user_choice():
-    valid_answers = ["rock","paper","scissors"]
+    valid_answers = ["rock","paper","scissors"] # String of potential inputs to complete a validity check against
     while True:
-        user_choice = input("Please input of one of the following, rock, paper or scissors")
-        if user_choice.lower() not in valid_answers:
-            print("Invalid input, please re-enter your answer")
+        user_choice = str(input("Please input of one of the following, rock, paper or scissors")) # Asks the user for a string input
+        if user_choice.lower() not in valid_answers: # Checks the input is in the valid_answers list to ensure a valid input
+            print("Invalid input, please re-enter your answer") # Does not break the loop and asks for another input
         else:
-            return user_choice
+            return user_choice # If the input passes the valid check, then the input will be returned
 
 
-def get_winner(computer_choice, user_choice):
-    print(computer_choice)
-    print(user_choice)
-    if computer_choice == user_choice:
+def get_winner(computer_choice, user_choice): # Function passes the computer and user choice as arguments 
+    print(f"Your choice was {user_choice}") 
+    print(f"The computer selected {computer_choice}") 
+    if computer_choice == user_choice: # Checks whether the user or computer won the game based on the inputs provided in the choice functions
         print("Both players selected the same choice, please select again")
     elif computer_choice == "rock":
         if user_choice == "paper":
@@ -42,6 +42,6 @@ def get_winner(computer_choice, user_choice):
 
 
 def play():
-    get_winner(get_computer_choice(), get_user_choice())
+    get_winner(get_computer_choice(), get_user_choice()) # This function calls the game functions and passes the computer choice and user choice function as arguments
 
 play()
